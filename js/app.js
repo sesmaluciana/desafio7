d7b88adec43ea766f91ce4d3e465fd93ad67fd58
sesmaluciana <sesmaluciana9@gmail.com>
2024-06-13 15:18:01 -0300

    hice el desafio 7

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const precioProducto =document.getElementById('precioProducto');
+const nombreProducto =document.getElementById('nombreProducto')
+
+
+precioProducto.innerText = "Precio = $450"
+console.log(precioProducto);
+
+const titulo =document.querySelector('#nombreProducto')
+titulo.style.color ="lightblue"
+titulo.style.fontSize="50px"
